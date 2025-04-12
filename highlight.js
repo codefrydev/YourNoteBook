5b2b83b8a303b9f601b09686152938eca8ba380c
PrashantUnity <48068632+PrashantUnity@users.noreply.github.com>
2025-04-12 12:07:44 +0000

    Deploying to gh-pages from  @ 63fb6fc83555a496615fd2a959a8986b57cd19d7 🚀

---
+++ highlight.js
@@ -0,0 +1,14 @@
+window.downloadFile = (fileName, content) => {
+    const blob = new Blob([content], { type: 'text/markdown' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+};
+
+// Initialize highlight.js
+hljs.highlightAll(); 
